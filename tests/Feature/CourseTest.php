cb32702878a06ded9c39fe24451297f381d2ead6
radwa-gamal30 <radwagamal482@gmail.com>
2025-05-21 12:12:07 +0300

    radwa

--- tests/Feature/CourseTest.php
+++ tests/Feature/CourseTest.php
@@ -3,8 +3,9 @@
 namespace Tests\Feature;
 
 use App\Models\User;
+use Spatie\Permission\Models\Permission;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+// use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class CourseTest extends TestCase
@@ -12,8 +13,9 @@ class CourseTest extends TestCase
    use RefreshDatabase;
    public function test_admin_can_create_course()
     {
-        $admin = User::factory()->create(['is_admin' => true]);
-
+        $admin = User::factory()->create(['is_admin' => 'yes']);
+        Permission::create(['name' => 'create_course']); 
+        $admin->givePermissionTo('create_course');
         $response = $this->actingAs($admin)->post('/courses', [
             'title' => 'Learing System management',
             'description' => 'system to handle learning operation.',
@@ -24,7 +26,7 @@ class CourseTest extends TestCase
     }
     public function test_user_cannot_create_course()
     {
-        $user = User::factory()->create(['is_admin' => false]);
+        $user = User::factory()->create(['is_admin' => 'no']);
 
         $response = $this->actingAs($user)->post('/courses', [
             'title' => 'Learing System management',
